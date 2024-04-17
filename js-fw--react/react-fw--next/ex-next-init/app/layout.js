107fc49913291b6d9ff207ebf7528b28daffe495
yjj <yoojj.dev@gmail.com>
2024-04-18 00:27:40 +0900

    next

---
+++ js-fw--react/react-fw--next/ex-next-init/app/layout.js
@@ -0,0 +1,14 @@
+//import './style.css'
+
+export const metadata = {
+  title: 'zzzz'
+};
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
